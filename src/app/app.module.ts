03802a3bf4b1d0b35ba429ae40e53744c506ee85
Umut Ocak <48753655+umutocak@users.noreply.github.com>
2021-05-14 15:09:47 +0300

    Add search pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ProductComponent } from './components/product/product.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { CategoryComponent } from './components/category/category.component';
 import { HttpClientModule } from '@angular/common/http';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,14 @@ import { HttpClientModule } from '@angular/common/http';
     ProductComponent,
     NaviComponent,
     CategoryComponent,
+    VatAddedPipe,
+    FilterPipePipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
